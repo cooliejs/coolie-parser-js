80f583152114a6badfbff5cdeaae4e97ce9e05a1
#云淡然 <cloudcome@163.com>
2018-12-05 11:36:37 +0800

    + uglifyjs@3

--- src/index.js
+++ src/index.js
@@ -10,14 +10,41 @@
 
 
 var object = require('blear.utils.object');
+var uglify = require('uglify-js');
 
 var defaults = {
-
+    /**
+     * 处理 require()
+     * @param node
+     * @returns {*}
+     */
+    processRequire: function (node) {
+        return node;
+    }
+};
+var pipe = function (ast, transform) {
+    var transformer = new uglify.TreeTransformer(null, transform);
+    ast.transform(transformer);
+    return ast;
 };
 
 module.exports = function (js, options) {
     options = object.assign({}, defaults, options);
 
-
+    var ast = uglify.parse(js, {});
+    ast.source = js;
+    pipe(ast, function (node) {
+        if (
+            node.start.type === 'name' &&
+            node.start.value === 'require' &&
+            node.body &&
+            node.body.args &&
+            node.body.expression
+        ) {
+            return options.processRequire(node);
+        }
+    });
+    return ast;
 };
-
+module.exports.uglify = uglify;
+module.exports.pipe = pipe;
--- test/test.index.js
+++ test/test.index.js
@@ -8,11 +8,101 @@
 'use strict';
 
 var expect = require('chai-jasmine').expect;
-var render = require('../src/index.js');
+var parser = require('../src/index.js');
 var fs = require('fs');
 var path = require('path');
+var uglify = require('uglify-js');
 
-describe('index', function () {
+describe('coolie-parser-js', function () {
+
+    it('processRequire sync', function () {
+        var ast = parser('require("a")', {
+            processRequire: function (node) {
+                var arg0 = node.body.args[0];
+                var prop = node.body.expression.property;
+                expect(prop).toBe(undefined);
+                expect(node.body.args.length).toBe(1);
+                expect(arg0.value).toBe('a');
+                expect(arg0.quote).toBe('"');
+                node.body.args[0].value = 'A';
+                return node;
+            }
+        });
+        var example2 = ast.print_to_string({beautify: true, comments: 'all'});
+        expect(example2).toEqual('require("A");');
+    });
+
+    it('processRequire pipeline', function () {
+        var ast = parser('require("a", \'b\')', {
+            processRequire: function (node) {
+                var arg0 = node.body.args[0];
+                var prop = node.body.expression.property;
+                expect(prop).toBe(undefined);
+                expect(node.body.args.length).toBe(2);
+                expect(arg0.value).toBe('a');
+                expect(arg0.quote).toBe('"');
+                node.body.args[0].value = 'A';
+                node.body.args[1].value = 'B';
+                return node;
+            }
+        });
+        var example2 = ast.print_to_string({beautify: true, comments: 'all'});
+        expect(example2).toEqual('require("A", "B");');
+    });
+
+    it('processRequire async', function () {
+        var ast = parser('require.async("a")', {
+            processRequire: function (node) {
+                var arg0 = node.body.args[0];
+                var prop = node.body.expression.property;
+                expect(prop).toBe('async');
+                expect(node.body.args.length).toBe(1);
+                expect(arg0.value).toBe('a');
+                expect(arg0.quote).toBe('"');
+                node.body.args[0].value = 'A';
+                return node;
+            }
+        });
+        var example2 = ast.print_to_string({beautify: true, comments: 'all'});
+        expect(example2).toEqual('require.async("A");');
+    });
+
+    it('processRequire ref', function () {
+        var ast = parser('var a = 1; require(a, "b");', {
+            processRequire: function (node) {
+                var arg0 = node.body.args[0];
+                var prop = node.body.expression.property;
+                expect(arg0.TYPE).not.toEqual('String');
+                return node;
+            }
+        });
+        var example2 = ast.print_to_string({beautify: true, comments: 'all'});
+        expect(example2).toEqual('var a = 1;\n\nrequire(a, "b");');
+    });
+
+    it('pipe method', function () {
+        var example = fs.readFileSync(path.join(__dirname, 'example.js'), 'utf8');
+        var ast = parser(example);
+        parser.pipe(ast, function (node) {
+            if (
+                node.start.type === 'name' &&
+                node.start.value === 'require' &&
+                node.body &&
+                node.body.args &&
+                node.body.expression
+            ) {
+                var arg0 = node.body.args[0];
+                var prop = node.body.expression;
+                node.body.args[0].value = '<<' + arg0.value + '>>';
+            }
+
+            return node;
+        });
+        var example2 = ast.print_to_string({beautify: true, comments: 'all'});
+        expect(example2).toMatch(/<<d>>/);
+        expect(example2).toMatch(/<<e>>/);
+        expect(example2).toMatch(/<<g>>/);
+    });
 
 });
 
